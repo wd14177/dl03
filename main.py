d7e06ea825099e7e5ba98d5f20ce9fe98c580010
wd14177 <wd14177@gmail.com>
2026-02-25 12:29:43 +0900

    0225-6

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, HTTPException
+import torch
+from torchvision import transforms, models
+import torch.nn as nn
+import torchvision.transforms as transforms
+import torchvision.models as models
+import json
+import uuid
+import os
+import shutil
 
 print("Hello, FastAPI!")
 
@@ -6,5 +15,26 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "World!!"}
 
+@app.get("/infer") # body : form-data
+def infer(file: UploadFile = File(...)):
+    allowed_ext = ['.jpg', '.jpeg', '.png', '.webp']
+    ext = file.filename.split(".")[-1].lower()
+
+    if ext not in allowed_ext:
+        return {"error" : "Invalid file type. Only .jpg, .jpeg, .png, and .webp are allowed."}
+    
+    newfile_name = f"{uuid.uuid4()}.{ext}"
+    file_path = os.path.join("upload_img", newfile_name)
+    
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+#---------------------추론하기---------------------
+# 사용자 데이터는 저장한다
+# 향후 재사용 함을 공지한다
+    
+    return {"result": "class1", "index" : "2"}
+
+    # 데이터 수집 --> 전처리 --> 학습 --> 테스트 --> 테스트 --> 추론 --> 피드백(--> 데이터 수집)
